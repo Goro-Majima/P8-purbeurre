d721091a4b0a4e5f010abb8a3a0b0f76a876df1a
Jiomod <lymickael91@gmail.com>
2019-08-18 22:28:39 +0200

    up

--- purbeurre/users/forms.py
+++ purbeurre/users/forms.py
@@ -3,7 +3,16 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 class UserRegisterForm(UserCreationForm):
+    username = forms.CharField(label='Login')
     email = forms.EmailField()
+    password1 = forms.CharField(label='Mot de passe')
+    password2 = forms.CharField(label='Confirmation mot de passe')
+    
+    def __init__(self, *args, **kwargs):
+        super(UserRegisterForm, self).__init__(*args, **kwargs)
+
+        for fieldname in ['username', 'password1', 'password2']:
+            self.fields[fieldname].help_text = None
 
     class Meta:
         model = User
