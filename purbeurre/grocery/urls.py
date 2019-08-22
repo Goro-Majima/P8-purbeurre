1828e7871c0e12f8aef3ac6610ff41a916e0061a
Jiomod <lymickael91@gmail.com>
2019-08-22 19:55:14 +0200

    autocomplete form ok, results half way thru

--- purbeurre/grocery/urls.py
+++ purbeurre/grocery/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     re_path(r'^$', views.home, name='home'),
     re_path(r'^results', views.results, name='results'),
     re_path(r'^mentions', views.mentions, name='mentions'),
+    re_path(r'^ajax_calls/search/', views.autocompleteModel),
 ]
--- purbeurre/grocery/views.py
+++ purbeurre/grocery/views.py
@@ -1,12 +1,37 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from grocery.models import Category, Product
+import json
 
 def home(request):
     return render(request, 'grocery/home.html')
 
 def results(request):
-    return render(request, 'grocery/results.html')
+    if request.method == 'GET':
+        text = request.GET.get('txtSearch')
+        product = Product.objects.filter(name__startswith=text).first()
+        context = {
+            'product_name': product.name,
+            'product_image': product.image,
+            'product_nutrigrade': product.nutrigrade,
+            'product_nutrient': product.nutrient,
+            'product_url': product.url,
+        }
+    return render(request, 'grocery/results.html', context)
 
 def mentions(request):
     return render(request, 'grocery/mentions.html')
+
+def autocompleteModel(request):
+    if request.is_ajax():
+        q = request.GET.get('term', '').capitalize()
+        search_qs = Product.objects.filter(name__startswith=q)
+        results = []
+        # print(q)
+        for r in search_qs:
+            results.append(r.name)
+        data = json.dumps(results)
+    else:
+        data = 'fail'
+    mimetype = 'application/json'
+    return HttpResponse(data, mimetype)
