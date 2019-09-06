f387508285cf7576e5380e3a43610f40360b7556
Jiomod <lymickael91@gmail.com>
2019-09-06 10:43:33 +0200

    'up'

--- purbeurre/users/models.py
+++ purbeurre/users/models.py
@@ -4,5 +4,5 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     """"pip install pillow to get ImageField, image set by default when profile is created"""
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
