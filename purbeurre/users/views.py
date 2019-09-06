2bac39605d2a08f3893afaf96010c56baead8ffe
Jiomod <lymickael91@gmail.com>
2019-09-06 12:51:58 +0200

    up

--- purbeurre/users/views.py
+++ purbeurre/users/views.py
@@ -11,7 +11,7 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Your account has been created! You are now able to login {username}!')
+            messages.success(request, f'Votre compte est créé {username}!')
             return redirect('login')
     else:
         form = UserRegisterForm()
