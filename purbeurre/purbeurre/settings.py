d592e0d7594d9015703b829b6f92059a0789d26b
Jiomod <lymickael91@gmail.com>
2019-09-16 15:21:16 +0200

    up

--- purbeurre/purbeurre/settings.py
+++ purbeurre/purbeurre/settings.py
@@ -100,7 +100,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # on utilise l'adaptateur postgresql
         'NAME': 'purbeurre', # le nom de notre base de données créée précédemment
-        'USER': 'mickael', # attention : remplacez par votre nom d'utilisateur !!
+        'USER': 'postgres', # attention : remplacez par votre nom d'utilisateur !!
         'PASSWORD': 'Lyteemo5',
         'HOST': 'localhost',
         'PORT': '5432',
