7b71e2f80e81adc0c45a15d2b6fb87f44111a041
mickael <lymickael91@gmail.com>
2019-09-19 07:03:05 +0000

    up

--- purbeurre/purbeurre/settings/production.py
+++ purbeurre/purbeurre/settings/production.py
@@ -1,10 +1,10 @@
 from .import *
 
-SECRET_KEY ="?_Xd'_H}VP[U:-^36$IoK:y\x0c"
-DEBUG= False
+SECRET_KEY = "?_Xd'_H}VP[U:-^36$IoK:y\x0c"
+DEBUG = False
 ALLOWED_HOSTS = ['157.245.72.19']
 
-DATABASES ={
+DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql', # on utilise l'adaptateur postgresql
             'NAME': 'purbeurre', # le nom de notre base de données créée précédemment
